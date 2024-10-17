0f408d1dfcd56314bbdcb66e0493cff7b3683696
aemc64 <aemc.64@gmail.com>
2024-10-17 18:26:38 +0200

    Updated DoorButton.

--- Assets/Scripts/DoorButton.cs
+++ Assets/Scripts/DoorButton.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class DoorButton : MonoBehaviour
 {
     [SerializeField] private Door _door;
+    [SerializeField] private Collider _trigger;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             _door.Open();
+            _trigger.enabled = false; // Disables the trigger to prevent calling Door.Open multiple times
         }
     }
 }
